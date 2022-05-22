16f275a9a6432a2b1a7f1d92b1e884c7065b0c9a
XHMX04 <2571803489@qq.com>
2022-05-22 13:29:49 +0800

    添加控制器

---
+++ shop-service/src/main/java/com/we/shopservice/controller/CommodityController.java
@@ -0,0 +1,11 @@
+package com.we.shopservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 莫
+ */
+@RestController
+public class CommodityController {
+
+}
