45f1e1e39f71e691af5502657d8851b07ce993ff
Ran <redamancy0326@foxmail.com>
2022-05-22 14:48:40 +0800

    Controller 第一次复写

---
+++ shop-service/src/main/java/com/we/shopservice/config/ResultMap.java
@@ -0,0 +1,59 @@
+package com.we.shopservice.config;
+
+import lombok.*;
+
+/**
+ * @author wRan
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultMap<T> {
+    private String code;
+    private String message;
+    private T data;
+
+    public ResultMap(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static ResultMap<?> build(Status status) {
+        return new ResultMap<>(status.getCode(), status.getReason());
+    }
+
+    public static <T> ResultMap<T> build(Status status, T data) {
+        return new ResultMap<>(status.getCode(), status.getReason(), data);
+    }
+
+    public static <T> ResultMap<T> build(Status status, String message, T data) {
+        return new ResultMap<>(status.getCode(), message, data);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ToString
+    public enum Status {
+        /**
+         * 100-200 : 成功状态码
+         * 200+ : 失败状态码
+         * {
+         *     SUCCESS:未出现错误的唯一返回状态码
+         *
+         * }
+         */
+        SUCCESS("200", "成功！"),
+        LOGIN_SUCCESS("201", "登录成功！"),
+        FAIL("100", "未知失败！"),
+        LOGINFAIL("101", "用户名或密码错误！"),
+        SESSION_EXPIRED("102", "会话到期！"),
+        INSUFFICIENT_PERMISSIONS("103", "权限不足！"),
+        TOKEN_EXCEPTION("104", "Token异常！"),
+        NOT_LOGGED_IN("105", "未登录！"),
+        DUPLICATE_ACCOUNT("106", "账号重复！");
+
+        private final String code;
+        private final String reason;
+
+    }
+}
--- shop-service/src/main/java/com/we/shopservice/controller/CommodityController.java
+++ shop-service/src/main/java/com/we/shopservice/controller/CommodityController.java
@@ -1,16 +1,34 @@
 package com.we.shopservice.controller;
 
+import com.we.shopservice.config.ResultMap;
+import com.we.shopservice.entity.Commodity;
+import com.we.shopservice.service.impl.CommodityServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author 莫
  */
 @RestController
+@RequestMapping("/commodity")
 public class CommodityController {
-    @RequestMapping("/")
-    public String hello(){
-        return "hello";
+
+    final CommodityServiceImpl commodityService;
+
+    public CommodityController(CommodityServiceImpl commodityService) {
+        this.commodityService = commodityService;
+    }
+
+    /**
+     * @return 全部的商品
+     */
+    @RequestMapping("/getAll")
+    public ResultMap<?> getAll(){
+        List<Commodity> list = commodityService.list();
+        return ResultMap.build(ResultMap.Status.SUCCESS,list);
     }
 
 }
